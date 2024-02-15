485c0ad91bb1faf5b8c9f7bcee08aea699b7df79
Ghadamsm <68555859+Ghadamsm@users.noreply.github.com>
2024-02-15 18:33:08 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -33,23 +33,23 @@ public class Main {
 
 
 
-//                int[] array = {1, 4, 17, 7, 25, 3, 100};
-//
-//                double sum = 0;
-//                for (int i = 0; i < array.length; i++) {
-//                    sum += array[i];
-//                }
-//                double average = sum / array.length;
-//
-//        System.out.print("The average of the said array is : " + average);
-//
-//        System.out.println (" The numbers in the said array that are greater than the average are : ");
-//
-//                for (int i = 0; i < array.length; i++) {
-//                    if (array[i] > average) {
-//                        System.out.print(array[i] + " ");
-//                    }
-//                }
+                int[] num = {1, 4, 17, 7, 25, 3, 100};
+
+                double sum = 0;
+                for (int i = 0; i < num.length; i++) {
+                    sum += num[i];
+                }
+                double average = sum / num.length;
+
+        System.out.print("The average of the said array is : " + average + " The numbers in the said " +
+                "array that are greater than the average are : ");
+
+
+                for (int i = 0; i < num.length; i++) {
+                    if (num[i] > average) {
+                        System.out.print(num[i] + " ");
+                    }
+                }
 
 
 
